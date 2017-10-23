f6b5809bc713bb79d6a6ae2c511f354a0a9e2064
Jake Robbins <jrobbinsd@gmail.com>
2017-10-23 14:50:41 -0600

    added redirect for home page

--- app.js
+++ app.js
@@ -15,7 +15,12 @@ app.get('/login', (req, res) => {
 	res.render('login.hbs')
 })
 
-
+app.post('/', (req, res)=>{
+	queries.login(req.body.code)
+	.then(user => {
+		res.redirect('/homepage/' + user[0].id)
+	})
+})
 
 app.listen(port, () => {
 	console.log(`listening at ${port}`);
