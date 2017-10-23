8e22c5d1b8b9620a799adc925a0d12ff2275ce42
Jake Robbins <jrobbinsd@gmail.com>
2017-10-23 14:55:10 -0600

    forgot a query var

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000
 const bodyParser = require('body-parser')
+const queries = require('./db/queries')
 
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
