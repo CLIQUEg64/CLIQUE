0ef04dca8f0bfba30a7e7a734a53eaaacace35ab
Makayla Clausen <mclausen792@gmail.com>
2017-10-23 15:05:48 -0600

    deployed to heroku

--- app.js
+++ app.js
@@ -1,17 +1,19 @@
 const express = require('express')
 const app = express()
-const port = 3000
+const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
 
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
-app.use(bodyParser.urlencoded({ extended: false}))
+app.use(bodyParser.urlencoded({
+  extended: false
+}))
 app.use(bodyParser.json())
 
-app.get('/', function (req, res) {
+app.get('/', function(req, res) {
   res.render('index');
 })
 
 app.listen(port, () => {
-	console.log(`listening at ${port}`);
+  console.log(`listening at ${port}`);
 })
--- knexfile.js
+++ knexfile.js
@@ -10,33 +10,8 @@ module.exports = {
     }
   },
 
-  staging: {
-    client: 'postgresql',
-    connection: {
-      database: 'my_db',
-      user: 'username',
-      password: 'password'
-    },
-    pool: {
-      min: 2,
-      max: 10
-    },
-    migrations: {
-      tableName: 'knex_migrations'
-    }
-  },
-
   production: {
-    client: 'postgresql',
-    connection: 'process.env.DATABASE_URL',
-    user: 'username',
-    password: 'password'
-  },
-  pool: {
-    min: 2,
-    max: 10
-  },
-  migrations: {
-    tableName: 'knex_migrations'
+    client: 'pg',
+    connection: process.env.DATABASE_URL
   }
 }
