dddcd024d56729b6452f71b5901f57db185e7622
Makayla Clausen <mclausen792@gmail.com>
2017-10-23 11:17:00 -0600

    Working on deployment

---
+++ knexfile.js
@@ -0,0 +1,44 @@
+// Update with your config settings.
+
+module.exports = {
+
+  development: {
+    client: 'pg',
+    connection: {
+      host: 'localhost',
+      database: 'clique'
+    }
+  },
+
+  staging: {
+    client: 'postgresql',
+    connection: {
+      database: 'my_db',
+      user: 'username',
+      password: 'password'
+    },
+    pool: {
+      min: 2,
+      max: 10
+    },
+    migrations: {
+      tableName: 'knex_migrations'
+    }
+  },
+
+  production: {
+    client: 'postgresql',
+    connection: 'process.env.DATABASE_URL',
+    user: 'username',
+    password: 'password'
+  },
+  pool: {
+    min: 2,
+    max: 10
+  },
+  migrations: {
+    tableName: 'knex_migrations'
+  }
+}
+
+};
