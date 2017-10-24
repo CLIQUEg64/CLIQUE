cc21f8296db63dc03b53071ee93fa9805e35c8dd
Jake Robbins <jrobbinsd@gmail.com>
2017-10-24 08:55:51 -0600

    working on homepage query

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000
 const bodyParser = require('body-parser')
-const queries = require('./db/queries')
+const queries = require('./db/queries');
 
 app.set('view engine', 'hbs');
 app.use(express.static('public'));
@@ -21,7 +21,8 @@ app.get('/login', (req, res) => {
 app.post('/', (req, res)=>{
 	queries.login(req.body.code)
 	.then(user => {
-		res.redirect('/homepage/' + user[0].id)
+		res.redirect('/homepage/' + user[1].id)
+		console.log(req.body.code)
 	})
 })
 
