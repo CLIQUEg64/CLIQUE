156c6b23fb57171d44449c04a5e6bce55ccde08a
Jake Robbins <jrobbinsd@gmail.com>
2017-10-26 11:36:49 -0600

    contact info

--- public/main.js
+++ public/main.js
@@ -1,15 +1,10 @@
-console.log('working?');
-
  var info = document.getElementById('edit')
  console.log(info)
-
- var nonhidden;
 
  info.addEventListener('click', function(e) {
    event.preventDefault()
    var info = document.getElementById('info')
    info.classList.add('editUser')
-   var editInfo = document.getElementsByClassName('editUser')[1]; console.log(editInfo)
+   var editInfo = document.getElementsByClassName('editUser')[1]
   editInfo.classList.remove('editUser');
 })
-  
