d76bbcfaadd569fbb2faeed7ca005190928b844f
Paul Rodriguez <paul@prdesign.work>
2017-10-26 14:06:13 -0600

    Fixed the quotes

--- public/quote.js
+++ public/quote.js
@@ -1,17 +1,23 @@
-var quoteOfTheDay = document.createElement('P');
-document.getElementsByClassName("quote")[0].append(quoteOfTheDay);
+var quoteRandom = document.createElement('P');
+document.getElementsByClassName("quote")[0].append(quoteRandom);
 var quoteAuthor = document.createElement('H4');
 document.getElementsByClassName("quote")[0].append(quoteAuthor);
 
-fetch('http://quotes.rest/qod.json')
+var myInit = { method: 'GET',
+               headers:
+               {'X-Theysaidso-Api-Secret':'_hCO1cAVkDiY_ScnlpegCAeF'}
+ }
+
+fetch('http://quotes.rest/quote/random.json', myInit)
 .then((response) => {
   return response.json()
   .then((quoteFromSite) => {
+console.log(quoteFromSite);
 
-    var quoteOfTheDay = quoteFromSite.contents.quotes["0"].quote;
-    var quoteAuthor = quoteFromSite.contents.quotes["0"].author;
+    var quoteRandom = quoteFromSite.contents.quote;
+    var quoteAuthor = quoteFromSite.contents.author;
 
-    document.getElementsByTagName('p')[0].innerHTML = '"' + quoteOfTheDay + '"';
+    document.getElementsByTagName('P')[0].innerHTML = '"' + quoteRandom + '"';
     document.getElementsByTagName('H4')[0].innerHTML = quoteAuthor;
   })
 })
