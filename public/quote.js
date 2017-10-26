f3232570a051593799082bb88f3ad21d06b70a17
Makayla Clausen <mclausen792@gmail.com>
2017-10-26 14:08:47 -0600

    Another heroku Deployment

--- public/quote.js
+++ public/quote.js
@@ -2,16 +2,16 @@ var quoteOfTheDay = document.createElement('P');
 document.getElementsByClassName("quote")[0].append(quoteOfTheDay);
 var quoteAuthor = document.createElement('H4');
 document.getElementsByClassName("quote")[0].append(quoteAuthor);
-
+console.log();
 fetch('http://quotes.rest/qod.json')
-.then((response) => {
-  return response.json()
-  .then((quoteFromSite) => {
+  .then((response) => {
+    return response.json()
+      .then((quoteFromSite) => {
 
-    var quoteOfTheDay = quoteFromSite.contents.quotes["0"].quote;
-    var quoteAuthor = quoteFromSite.contents.quotes["0"].author;
+        var quoteOfTheDay = quoteFromSite.contents.quotes["0"].quote;
+        var quoteAuthor = quoteFromSite.contents.quotes["0"].author;
 
-    document.getElementsByTagName('p')[0].innerHTML = '"' + quoteOfTheDay + '"';
-    document.getElementsByTagName('H4')[0].innerHTML = quoteAuthor;
+        document.getElementsByTagName('p')[0].innerHTML = '"' + quoteOfTheDay + '"';
+        document.getElementsByTagName('H4')[0].innerHTML = quoteAuthor;
+      })
   })
-})
