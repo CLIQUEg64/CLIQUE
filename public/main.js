93a6be6e5e8420820c751e1c0c60b20c183cf168
COLE CARROLL <YOUR EMAIL ADDRESS>
2017-10-26 11:25:41 -0600

    cleaned up main.js

--- public/main.js
+++ public/main.js
@@ -1,12 +1,10 @@
  var info = document.getElementById('edit')
  console.log(info)
 
- var nonhidden;
-
  info.addEventListener('click', function(e) {
    event.preventDefault()
    var info = document.getElementById('info')
    info.classList.add('editUser')
-   var editInfo = document.getElementsByClassName('editUser')[1];
+   var editInfo = document.getElementsByClassName('editUser')[1]
   editInfo.classList.remove('editUser');
 })
